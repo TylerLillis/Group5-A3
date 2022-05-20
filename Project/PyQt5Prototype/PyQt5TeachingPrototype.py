3f365abbedb30eefe015b9a998354e04469129e9
Moovlu <Epicmoo05@gmail.com>
2022-05-20 16:11:57 +1000

    Added basic prototype stuff, made changes to site, placed example and placeholder ui files and xml files.

---
+++ Project/PyQt5Prototype/PyQt5TeachingPrototype.py
@@ -0,0 +1,36 @@
+#Imports required modules
+from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5.QtWidgets import *
+import sys
+import os
+
+#Creates a class for the UI
+class Ui(QtWidgets.QMainWindow):
+
+    #Fixes PyQt5's weird DPI scaling for some laptops
+    if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)    
+    if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True) 
+
+    def __init__(self):
+        super(Ui, self).__init__()
+        #Loads the UI file from filename
+        uic.loadUi(os.path.join(sys.path[0], "ProgramUI.ui"), self)
+        self.show()
+        
+        #Connect elements to functions
+        self.ExamplePushbutton.clicked.connect(self.ExampleClicked)
+
+    #Functions behind UI elements
+    def ExampleClicked(self):
+        print('Example button was clicked')
+
+
+
+
+
+#Creates an instance of the app and window and then executes the program.
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
---
+++ Project/XMLParsingPrototype/XMLParsingPrototype.py
@@ -0,0 +1,21 @@
+'''
+Uses Python to parse module data from the data.xml file.
+I created an data.xml as an example file to parse data from.
+Note: Use loops to find the modules, DO NOT HARDCODE THE AMOUNT IT'S LOOKING FOR
+
+--Teaching Section--
+What needs to be parsed:
+- Module name
+- No. of sections
+- Completed sections
+
+And for each individual section:
+- Page number
+- Page layout type
+- Image links
+- Body of the page
+
+--Testing Section--
+What needs to be parsed:
+- Questions and answers from completed modules
+'''
