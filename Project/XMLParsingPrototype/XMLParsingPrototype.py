eb9fa5955569fbc4767e3a5da46189b70c6ac1e7
Moovlu <Epicmoo05@gmail.com>
2022-05-23 23:06:41 +1000

    Completed parsing prototype

--- Project/XMLParsingPrototype/XMLParsingPrototype.py
+++ Project/XMLParsingPrototype/XMLParsingPrototype.py
@@ -12,10 +12,40 @@ What needs to be parsed:
 And for each individual section:
 - Page number
 - Page layout type
-- Image links
 - Body of the page
 
 --Testing Section--
 What needs to be parsed:
 - Questions and answers from completed modules
 '''
+
+#Import required modules
+import xml.etree.ElementTree as et
+import os
+import sys
+
+tree = et.parse(os.path.join(sys.path[0], "Data.xml"))
+root = tree.getroot()
+
+#Parse required data for teaching section
+for module in root:
+    print('Module name:', module.attrib['name'])
+    print('Number of sections:', module[0].text)
+    print('Completed?:', module[1].text)
+
+    SectionNum = 1
+    for section in module[2]:
+        print('   Section', SectionNum)
+        SectionNum += 1
+        for page in section[0]:
+            print('Page number:',page.attrib['num'])
+            print('Page layout:', page[0].text)
+            print('Page body:', page[1].text)
+    print('\n')
+    
+#Parse required data for testing section
+for module in root:
+    if module[1].text == 'Yes':
+        for question in module[3]:
+            print('Question:', question[0].text)
+            print('Answer:', question[1].text)
